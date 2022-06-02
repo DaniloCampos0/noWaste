7bcc3fd31170d7bf8aa0f11b3be237b33bf88d4a
Carlos Rocha <carlosrocha2014rochacarlos@outlook.com>
2022-06-02 18:49:46 -0300

    Primeiro commit

---
+++ noWaste-main/formContato.php
@@ -0,0 +1,57 @@
+<?php
+require_once ('conexao.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+    
+    
+</head>
+      
+<body>
+    <form>
+    <div class="form-group">
+        <label for="userEmail">Nome</label>
+        <input type="text" name="u_nome" id="userNome" placeholder="Nome" required><br><br>
+    </div>
+    <div class="form-group">
+        <label for="userEmail">E-mail</label>
+        <input type="email" name="u_email" id="userEmail" placeholder="example@email.com.br" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required><br><br>
+    </div>    
+    <div class="form-group">
+        <label for="telefone">Telefone</label>
+        <input type="text" name="telefone" id="telefone"  placeholder="(00)0000-0000" minlenght="10" pattern="[0-9]+$" maxlength="15" required><br><br>
+    </div>    
+        
+            <select>
+                <option id="residencial" value="residencial">Residencial</option>
+                <option id="comercial" value="comercial">Comercial</option>
+                <option id="celular" value="celular">Celular</option>
+            </select><br><br>
+
+        <div class="form-group">    
+            <label for="whatsapp">Whatsapp</label>
+            <input type="text" name="whatsapp" id="whatsapp"  placeholder="(00)0000-0000"  pattern="[0-9]+$" minlenght="10" required><br><br>
+    </div>    
+                        
+    <div class="form-group">
+            <label for="redeSocial">Rede Social</label>
+            <input type="text" name="redeSocial" id="redeSocial"  placeholder="Informe sua rede social para contato" required><br><br>
+    </div>
+    <div class="form-group">        
+            <input type="submit" class="form-control" name="salvarUsuario" id="salvarUsuario">>
+    </div>        
+    </form>
+
+
+
+ </body>
+ 
+</html>
